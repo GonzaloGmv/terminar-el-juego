b75e9df6206c2c82771cfc7a94cb41d76d35bc77
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2021-11-09 21:07:05 +0100

    cambio

--- terminar_el_juego.py
+++ terminar_el_juego.py
@@ -1,7 +1,7 @@
 import random
 def pedir_numero(MIN,MAX):
     while True:
-        respuesta = input("Escriba un numero entre " + str(MIN) + " y " + str(MAX))
+        respuesta = input("Escriba un numero entre " + str(MIN) + " y " + str(MAX) + ": ")
         try:
             respuesta = int(respuesta)
         except:
@@ -13,15 +13,28 @@ def pedir_numero(MIN,MAX):
 
 def adivinar(MIN, MAX, NUM):
     while True:
-        intento = pedir_numero(MIN, MAX)
-        if intento < NUM:
-            print("Te has quedado corto")
-        elif intento > NUM:
-            print("Te has pasado")
+        ayuda = input("Si desea ayuda para adivinar el numero escriba SI, si no la desea escriba NO: ")
+        try:
+           ayuda = str(ayuda)
+        except:
+            pass
         else:
+            if ayuda == "SI":
+                while True:
+                    intento = pedir_numero(MIN, MAX)
+                    if intento < NUM:
+                        print("Te has quedado corto")
+                    elif intento > NUM:
+                        print("Te has pasado")
+                    else:
+                        break
+            elif ayuda == "NO":
+                while True:
+                    intento = pedir_numero(MIN, MAX)
+                    if intento == NUM:
+                        break
             print("Has acertado")
             break
-    return intento
 
 print("1 - Nivel simple")
 print("2 - Nivel intermedio")
